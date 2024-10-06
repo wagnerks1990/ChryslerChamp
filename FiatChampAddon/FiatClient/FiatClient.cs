59d65da297e9e90c502d1e2810bb1b13b6c01256
wagnerks1990 <31423401+wagnerks1990@users.noreply.github.com>
2024-10-06 01:48:16 -0400

    Update FiatClient.cs

--- FiatChampAddon/FiatClient/FiatClient.cs
+++ FiatChampAddon/FiatClient/FiatClient.cs
@@ -287,7 +287,8 @@ public enum FcaBrand
   Ram,
   Jeep,
   Dodge,
-  AlfaRomeo
+  AlfaRomeo,
+  Chrysler
 }
 
 public enum FcaRegion
@@ -342,6 +343,18 @@ public class FiatClient : IFiatClient
       _loginApiKey = "3_etlYkCXNEhz4_KJVYDqnK1CqxQjvJStJMawBohJU2ch3kp30b0QCJtLCzxJ93N-M";
       _apiKey = "OgNqp2eAv84oZvMrXPIzP8mR8a6d9bVm1aaH9LqU";
       _loginUrl = "https://login-us.dodge.com";
+      _tokenUrl = "https://authz.sdpr-02.fcagcv.com/v2/cognito/identity/token";
+      _apiUrl = "https://channels.sdpr-02.fcagcv.com";
+      _authApiKey = "JWRYW7IYhW9v0RqDghQSx4UcRYRILNmc8zAuh5ys"; // UNKNOWN
+      _authUrl = "https://mfa.fcl-01.fcagcv.com"; // UNKNOWN
+      _awsEndpoint = RegionEndpoint.USEast1;
+      _locale = "en_us";
+    }
+    else if (_brand == FcaBrand.Chrysler)
+    {
+      _loginApiKey = "3_etlYkCXNEhz4_KJVYDqnK1CqxQjvJStJMawBohJU2ch3kp30b0QCJtLCzxJ93N-M";
+      _apiKey = "OgNqp2eAv84oZvMrXPIzP8mR8a6d9bVm1aaH9LqU";
+      _loginUrl = "https://login-us.Chrysler.com";
       _tokenUrl = "https://authz.sdpr-02.fcagcv.com/v2/cognito/identity/token";
       _apiUrl = "https://channels.sdpr-02.fcagcv.com";
       _authApiKey = "JWRYW7IYhW9v0RqDghQSx4UcRYRILNmc8zAuh5ys"; // UNKNOWN
